c17c198b3e496b1a3802ee4c9242b108a966dc52
shashask1 <shashask1@gmail.com>
2021-11-14 13:55:12 +0800

    combined

--- timer.js
+++ timer.js
@@ -1,47 +1,3 @@
-let myTaskList = document.getElementsByTagName("LI");
-for(let i=0; i<myTaskList.length;i++){
-    let span = document.createElement("SPAN");
-    let task = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(task);
-    myTaskList[i].appendChild(span);
-}
-
-let close = document.getElementsByClassName("close");
-for(let index=0; index< close.length; index++){
-    close[index].onclick = function(){
-        let div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-function newElement(){
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("task").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if(inputValue === ""){
-        alert("You Must write Something!");
-    } else {
-        document.getElementById("workList").appendChild(li);
-    }
-    document.getElementById("task").value = "";
-
-    let span = document.createElement("SPAN");
-    let task = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(task);
-    li.appendChild(span);
-
-    for(let index=0; index< close.length; index++){
-        close[index].onclick = function(){
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-
-
-}
 // Convert time to a format of hours, minutes, seconds, and milliseconds
 
 function timeToString(time) {
@@ -116,4 +72,3 @@ function timeToString(time) {
   playButton.addEventListener("click", start);
   pauseButton.addEventListener("click", pause);
   resetButton.addEventListener("click", reset);
-//   end of timer script 
